a068617c870ce7b31ffff83452f8ba2503fe4607
Josh <zylo.codes@gmail.com>
2023-05-06 09:54:59 -0500

    constants

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,30 @@ use postgres::Error as PostgresError;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String
+}
+
+const DB_URL : &str = !env("DATABASE_URL");
+
+const OK_RESPONSE : &str = "HTTP/1.1 200 OK\r\nContent-Type: application/jsor\n\r\n";
+const NOT_FOUND : &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR : &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
+fn main() {
+    // set db
+    if let Err(e) = set_database() {
+        println!("Error: {}", e)
+    }
+}
+
+fn set_database() {
+
+}
